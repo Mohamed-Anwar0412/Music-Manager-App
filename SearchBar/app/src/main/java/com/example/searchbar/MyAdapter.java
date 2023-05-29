8f99775adb80dbcb661d823a599949ca774b6510
StarSage <129214435+StarSage1@users.noreply.github.com>
2023-05-29 23:19:33 +0300

    SearchBar

---
+++ SearchBar/app/src/main/java/com/example/searchbar/MyAdapter.java
@@ -0,0 +1,74 @@
+package com.example.searchbar;
+
+import android.media.MediaPlayer;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.io.IOException;
+import java.util.List;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    private List<String> items;
+    private OnItemClickListener onItemClickListener;
+
+    public void setItems(List<String> items) {
+        this.items = items;
+        notifyDataSetChanged();
+    }
+
+    public void clear() {
+        items = null;
+        notifyDataSetChanged();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(String itemName);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        this.onItemClickListener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String itemName = items.get(position);
+        holder.itemNameTextView.setText(itemName);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items != null ? items.size() : 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView itemNameTextView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            itemNameTextView = itemView.findViewById(R.id.textview);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                String itemName = items.get(position);
+                if (onItemClickListener != null) {
+                    onItemClickListener.onItemClick(itemName);
+                }
+            }
+        }
+    }
+}
